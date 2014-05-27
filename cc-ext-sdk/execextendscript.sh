1abcbf486c31a507ab0790c08113986cefab95ad
David Deraedt <dehats@gmail.com>
2014-05-27 12:53:42 +0200

    fixed paths

--- cc-ext-sdk/execextendscript.sh
+++ cc-ext-sdk/execextendscript.sh
@@ -5,15 +5,15 @@ appcode=$1
 if [ "$appcode" = "ps" ]
 then
     app="Adobe Photoshop CC"
-    command="do javascript \"#include $2\""
+    command="do javascript \"#include '$2'\""
 elif [ "$appcode" = "ai" ]
 then
     app="Adobe Illustrator"
-    command="do javascript \"#include $2\""
+    command="do javascript \"#include '$2'\""
 elif [ "$appcode" = "id" ]
 then 
     app="Adobe InDesign CC"
-    command="do script \"#include $2\" language javascript" 
+    command="do script \"#include '$2'\" language javascript" 
 elif [ "$appcode" = "ae" ]
 then
     app="Adobe After Effects CC"
