d43ffab5e670634c0bda4a4ac34f8e06bd785b58
David Deraedt <dehats@gmail.com>
2014-06-25 10:36:31 +0200

    removed legacy SDK

--- cc-ext-sdk/deployext.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set cepdir=CEP
-set dest=%APPDATA%\Adobe\%cepdir%\extensions\%2
-XCOPY "%1" %dest% /D /E /C /R /I /K /Y >nul
-echo %dest%
--- cc-ext-sdk/disabledebugmode.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/usr/libexec/PlistBuddy -c "Delete:PlayerDebugMode" ~/Library/Preferences/com.adobe.CSXS.5.plist
